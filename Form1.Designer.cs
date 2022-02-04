31e24d9b415f45b6b8ebeabb0588bd886d4b4e76
Pavel Cherepanov <80674099+DMlockerM@users.noreply.github.com>
2022-02-05 02:17:36 +1100

    SkinChecker v1.1
    New version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,6 +56,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Введите никнейм / Enter nickname";
             this.textBox1.Size = new System.Drawing.Size(273, 23);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -60,25 +64,59 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Location = new System.Drawing.Point(323, 501);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(153, 30);
             this.label1.TabIndex = 2;
-            this.label1.Text = "SkinChecker by DMlockerM\r\nVersion 1.0\r\n";
+            this.label1.Text = "SkinChecker by DMlockerM\r\nVersion 1.1";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 131);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 81);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(273, 450);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gotham Pro", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(322, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Выбрать / Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Items.AddRange(new object[] {
+            "Русский",
+            "English"});
+            this.comboBox1.Location = new System.Drawing.Point(322, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 23);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 612);
+            this.ClientSize = new System.Drawing.Size(488, 542);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -101,5 +139,8 @@
         private TextBox textBox1;
         private Label label1;
         private PictureBox pictureBox1;
+        private Label label2;
+        private Button button2;
+        private ComboBox comboBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-using System.Net;
+﻿using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace SkinChecker
 {
@@ -11,24 +12,75 @@ namespace SkinChecker
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            var text = File.ReadAllText("SkinChecker.json");
+            JObject json = JObject.Parse(text);
 
+            label2.Text = (string)json["locale"][(string)json["nowlang"]]["currlang"];
+            button1.Text = (string)json["locale"][(string)json["nowlang"]]["look"];
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text == "")
+
+            if (textBox1.Text == "")
             {
-                MessageBox.Show("Вы не ввели никнейм.");
+                var text = File.ReadAllText("SkinChecker.json");
+                JObject json = JObject.Parse(text);
+                MessageBox.Show((string)json["locale"][(string)json["nowlang"]]["err_nonick"], "err_nonick", MessageBoxButtons.OK, MessageBoxIcon.Error);
             } else
             {
                 string sURL = $"https://minotar.net/armor/body/{textBox1.Text}/210.png";
-                WebRequest req = WebRequest.Create(sURL);
-                WebResponse res = req.GetResponse();
-                Stream imgStream = res.GetResponseStream();
-                Image img1 = Image.FromStream(imgStream);
-                imgStream.Close();
-                pictureBox1.Image = img1;
+                try
+                {
+                    WebRequest req = WebRequest.Create(sURL);
+                    WebResponse res = req.GetResponse();
+                    Stream imgStream = res.GetResponseStream();
+                    Image img1 = Image.FromStream(imgStream);
+                    imgStream.Close();
+                    pictureBox1.Image = img1;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
 
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var text = File.ReadAllText("SkinChecker.json");
+            JObject json = JObject.Parse(text);
+
+            switch (comboBox1.Text)
+            {
+                case "":
+                    MessageBox.Show((string)json["locale"][(string)json["nowlang"]]["err_nochoicelang"], "err_nochoicelang", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case "Русский":
+                    json["nowlang"] = "RU";
+                    dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(text);
+                    jsonObj["nowlang"] = "RU";
+
+                    string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText("SkinChecker.json", output);
+                    label2.Text = (string)json["locale"]["RU"]["currlang"];
+                    button1.Text = (string)json["locale"]["RU"]["look"];
+                    break;
+                case "English":
+                    json["nowlang"] = "EN";
+                    dynamic jsonObj1 = Newtonsoft.Json.JsonConvert.DeserializeObject(text);
+                    jsonObj1["nowlang"] = "EN";
+
+                    string output1 = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj1, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText("SkinChecker.json", output1);
+                    label2.Text = (string)json["locale"]["EN"]["currlang"];
+                    button1.Text = (string)json["locale"]["EN"]["look"]; 
+                    break;
+                default:
+                    MessageBox.Show("O.O");
+                    break;
             }
 
         }
@@ -36,6 +88,31 @@ namespace SkinChecker
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
